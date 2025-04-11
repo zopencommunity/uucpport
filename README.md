17288284df7bc3cd30a67d25c01dc613446db7c8
Sachin T <sachintu47@gmail.com>
2025-04-11 05:32:19 -0400

    Add Initial patch

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/uucpport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/uucpport/actions/workflows/bump.yml)
+
+# uucp
+
+UUCP (Unix-to-Unix Copy)[1] is a suite of computer programs and protocols allowing remote execution of commands and transfer of files, email and netnews between computers.
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/uucpport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/uucpport.git'), string(name: 'PORT_DESCRIPTION', value: 'UUCP (Unix-to-Unix Copy)[1] is a suite of computer programs and protocols allowing remote execution of commands and transfer of files, email and netnews between computers.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
